3cf74e70e248a9e25c71c9528eee48936b687992
Per Juchtmans <dubgeiser@gmail.com>
2021-02-14 11:16:43 +0100

    Structure in templates dir and some CSS

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import request
 
 from countdown import generate_numbers
 
@@ -10,12 +12,16 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/numbers")
+@app.route("/numbers", methods=['GET', 'POST'])
 def numbers():
-    target, selection = generate_numbers()
-    return render_template("numbers.html", target=target, selection=selection)
+    if request.method == 'POST':
+        big_count = request.form['big_count']
+        target, selection = generate_numbers(big_count)
+        return render_template("numbers/index.html", target=target, selection=selection)
+    else:
+        return render_template("numbers/index.html")
 
 
 @app.route("/letters")
 def letters():
-    return render_template("letters.html")
+    return render_template("letters/index.html")
