9bf5588e45c09d289098134b69ccf65576358fa1
Per <per@dubgeiser.net>
2025-08-13 15:25:51 +0200

    Serving static files under `web/`, adding style

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,5 +11,6 @@ func main() {
 	http.HandleFunc("/", server.HomeHandler)
 	http.HandleFunc("/numbers/", server.NumbersHandler)
 	http.HandleFunc("/letters/", server.LettersHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web"))))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
